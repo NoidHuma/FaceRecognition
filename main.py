afc804b923123b2b2c4c109f40c84734ec265e89
Noid <nikitag.big2005@gmail.com>
2024-12-05 00:14:58 +0300

    create datatgens

--- main.py
+++ main.py
@@ -23,8 +23,29 @@ path = 'dataset/train'
 classes = os.listdir(path)
 print(classes)
 
+
 count = []
 for cat in classes:
     count.append(len(os.listdir(f'{path}/{cat}')))
 sb.barplot(x=classes, y=count)
 plt.show()
+
+
+train_datagen = ImageDataGenerator(rescale=1. / 255)
+val_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_gen = train_datagen.flow_from_directory(
+    'train',
+    target_size=(48, 48),
+    batch_size=64,
+    color_mode="grayscale",
+    class_mode='categorical')
+
+val_gen = val_datagen.flow_from_directory(
+    'test',
+    target_size=(48, 48),
+    batch_size=64,
+    color_mode="grayscale",
+    class_mode='categorical')
+
+
