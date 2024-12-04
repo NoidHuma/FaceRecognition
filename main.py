68f72a9704f596d02e9c8ec8723be5ba34b7ebf0
Noid <nikitag.big2005@gmail.com>
2024-12-05 00:11:56 +0300

    expore data

--- main.py
+++ main.py
@@ -16,5 +16,15 @@ from keras import layers
 from keras.preprocessing.image import ImageDataGenerator
 
 import warnings
+
 warnings.filterwarnings('ignore')
 
+path = 'dataset/train'
+classes = os.listdir(path)
+print(classes)
+
+count = []
+for cat in classes:
+    count.append(len(os.listdir(f'{path}/{cat}')))
+sb.barplot(x=classes, y=count)
+plt.show()
