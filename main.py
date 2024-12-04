baf3f809a4c4fcaddf481ac2f49b2e09bf204719
Noid <nikitag.big2005@gmail.com>
2024-12-04 17:25:20 +0300

    import libs

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+from PIL import Image
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from sklearn import metrics
 
+import cv2
+import os
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from glob import glob
 
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+from keras.preprocessing.image import ImageDataGenerator
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+import warnings
+warnings.filterwarnings('ignore')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
