68fa64816db20a36c25358bf197b2e87f9f6aaba
Noid <nikitag.big2005@gmail.com>
2024-12-05 23:41:30 +0300

    move model training to separate file

--- main.py
+++ main.py
@@ -1,88 +1,19 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sb
-from PIL import Image
-
-from sklearn import metrics
 
 import cv2
+
 import os
 
-from glob import glob
-
-import tensorflow as tf
-from tensorflow import keras
-from keras import layers
-from keras.preprocessing.image import ImageDataGenerator
-
-from keras.callbacks import EarlyStopping, ReduceLROnPlateau
-import tensorflow as tf
+from keras.models import load_model
 
 import warnings
 
 warnings.filterwarnings('ignore')
 
-train_path = 'dataset/train'
-val_path = 'dataset/test'
 
-train_datagen = ImageDataGenerator(rescale=1. / 255)
-val_datagen = ImageDataGenerator(rescale=1. / 255)
+model = load_model('my_model.keras')
 
-train_gen = train_datagen.flow_from_directory(
-    train_path,
-    target_size=(48, 48),
-    batch_size=64,
-    color_mode="grayscale",
-    class_mode='categorical')
-
-val_gen = val_datagen.flow_from_directory(
-    val_path,
-    target_size=(48, 48),
-    batch_size=64,
-    color_mode="grayscale",
-    class_mode='categorical')
-
-emotions = list(train_gen.class_indices.keys())
-print(emotions)
-
-model = keras.models.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(48, 48, 1)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D(2, 2),
-
-    layers.Flatten(),
-    layers.Dense(64, activation='relu'),
-    layers.BatchNormalization(),
-    layers.Dense(32, activation='relu'),
-    layers.Dropout(0.3),
-    layers.BatchNormalization(),
-    layers.Dense(7, activation='softmax')
-])
-
-model.compile(
-    optimizer='adam',
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
-
-
-class MyCallback(tf.keras.callbacks.Callback):
-    def on_epoch_end(self, epoch, logs=None):
-        if logs is None:
-            logs = {}
-        if logs.get('val_accuracy') > 0.90:
-            print('\nValidation accuracy has reached 90% so, stopping further training.')
-            self.model.stop_training = True
-
-
-es = EarlyStopping(patience=3, monitor='val_accuracy', restore_best_weights=True)
-lr = ReduceLROnPlateau(monitor='val_loss', patience=2, factor=0.5, verbose=1)
-
-history = model.fit(train_gen,
-                    validation_data=val_gen,
-                    epochs=5,
-                    verbose=1,
-                    callbacks=[es, lr, MyCallback()])
+classes = os.listdir("dataset/train")
 
 
 def plot_image(img, emoj):
